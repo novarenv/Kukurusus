dfbcaf724741cd7c5f37c3bbdea41f297a9d2e1d
Novaren Veraldo <novarenv@gmail.com>
2019-07-12 01:50:13 +0700

    Template Deskripsi Kursus Data

--- app/src/main/java/com/example/kukurusus/view/DescKursusAct.java
+++ app/src/main/java/com/example/kukurusus/view/DescKursusAct.java
@@ -17,8 +17,8 @@ import java.util.ArrayList;
 
 public class DescKursusAct extends AppCompatActivity {
     public static final String EXTRA_NAMA = "extra_nama";
-    ImageView ivBack;
-    TextView tvDNamaKursus;
+    ImageView ivBack, ivDesc, ivCertif;
+    TextView tvDNamaKursus, tvDesc, tvPrice, tvLocation, tvKuota, tvJadwal;
     Kursus kursus;
     Intent intent;
 
@@ -38,11 +38,29 @@ public class DescKursusAct extends AppCompatActivity {
         intent = this.getIntent();
         kursus = intent.getParcelableExtra("Kursus");
 
+        String gambarDetail = kursus.getPhoto();
+        ivDesc = findViewById(R.id.iv_detail_kursuss);
+        ivDesc.setImageResource(Integer.valueOf(gambarDetail));
         String nama = kursus.getName();
         tvDNamaKursus = findViewById(R.id.tv_d_nama_kursus);
         tvDNamaKursus.setText(nama);
         String desc = kursus.getDesc();
-        tvDNamaKursus = findViewById(R.id.tv_desc);
-        tvDNamaKursus.setText(desc);
+        tvDesc = findViewById(R.id.tv_desc);
+        tvDesc.setText(desc);
+        String price = kursus.getPrice();
+        tvPrice = findViewById(R.id.tv_price);
+        tvPrice.setText(price);
+        String location = kursus.getLocation();
+        tvLocation = findViewById(R.id.tv_location);
+        tvLocation.setText(location);
+        String certif = kursus.getCertif();
+        ivCertif = findViewById(R.id.iv_certif);
+        ivCertif.setImageResource(Integer.valueOf(gambarDetail));
+        String kuota = kursus.getKuota();
+        tvKuota = findViewById(R.id.tv_kuota);
+        tvKuota.setText(kuota);
+        String jadwal = kursus.getSchedule();
+        tvJadwal = findViewById(R.id.tv_jadwal);
+        tvJadwal.setText(jadwal);
     }
 }
