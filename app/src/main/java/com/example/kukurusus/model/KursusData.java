8dd461c3952a2edc2a3ac4681e82b22934f55dfc
Novaren Veraldo <novarenv@gmail.com>
2019-07-12 09:37:16 +0700

    Deskripsi Kursus Data

--- app/src/main/java/com/example/kukurusus/model/KursusData.java
+++ app/src/main/java/com/example/kukurusus/model/KursusData.java
@@ -16,12 +16,37 @@ public class KursusData implements Parcelable {
                     "Jl. Bahasain Aja 555", "Kami menyediakan jasa kursus semua bahasa yang ada di" +
                     " Eropa, dengan pengajar bersertifikasi internasional", "Certif", "Semua Bahasa " +
                     "Tersedia", "Senin - Jumat"},
-            {"Hehe", "Harga", Integer.toString(R.drawable.btn_code), "Lokasi", "Desc", "Certif", "Kuota", "Jadwal"},
-            {"Name", "Harga", Integer.toString(R.drawable.btn_code), "Lokasi", "Desc", "Certif", "Kuota", "Jadwal"},
-            {"Name", "Harga", Integer.toString(R.drawable.btn_code), "Lokasi", "Desc", "Certif", "Kuota", "Jadwal"},
-            {"Name", "Harga", Integer.toString(R.drawable.btn_code), "Lokasi", "Desc", "Certif", "Kuota", "Jadwal"},
-            {"Name", "Harga", Integer.toString(R.drawable.btn_code), "Lokasi", "Desc", "Certif", "Kuota", "Jadwal"},
-            {"Name", "Harga", Integer.toString(R.drawable.btn_code), "Lokasi", "Desc", "Certif", "Kuota", "Jadwal"}
+            {"Raveraldz's Code", "40.000", Integer.toString(R.drawable.btn_code), "Jl.Kampung Hacker 404",
+                    "Tempat belajar programming dengan pilihan bootcamp dan online course. Terdapat" +
+                    " lebih dari 10 bahasa pemrograman disertai framework dan design patternnya.",
+                    "Certif", "Semua Kelas Tersedia kecuali Flutter", "Setiap hari untuk online dan" +
+                    " Senin – Jumat kelas offline"},
+            {"Raveraldz's Cook", "Jl. Makan Kenyang 99", Integer.toString(R.drawable.btn_code),
+                    "Jl. Makan Kenyang 99", "Kami memberikan pelatihan masakan tradisional Indonesia" +
+                    " dengan campuran gaya eropa.", "Certif", "Tersisa 5", "Selasa - Kamis"},
+            {"Raveraldz's Drive", "300.000 per 5 pertemuan", Integer.toString(R.drawable.btn_code),
+                    "Jl. Raya Ban 10", "Kami menyediakan lahan latihan, kendaraan, serta jaminan " +
+                    "mendapatkan SIM untuk semua SIM A dan C.", "Certif", "SIM C tersedia, SIM A tersisa 10",
+                    "Setiap Hari"},
+            {"Raveraldz's Boutique Course", "300.000", Integer.toString(R.drawable.btn_code),
+                    "Jl. Cantique Bingit 69", "Kami menyediakan kursus bagi anda yang ingin membangun" +
+                    " butik anda, dari manajemen barang hingga bisnis.", "Certif", "Tersedia",
+                    "Sabtu - Minggu"},
+            {"Raveraldz's Elementary Course", "20.000", Integer.toString(R.drawable.btn_code),
+                    "Jl. Dasar Hidup 2", "Kami memberikan kursus pelajaran SD dan mempersiapkan untuk UN SD",
+                    "Certif", "Tersedia", "Senin - Jumat"},
+            {"Raveraldz's Junior High School Course", "40.000", Integer.toString(R.drawable.btn_code),
+                    "Jl. Tengah Tanggung", "Kami mengajarkan pelajaran SMP dengan jaminan masuk SMA negeri.",
+                    "Certif", "Tersedia", "JSenin - Jumat"},
+            {"Raveraldz's Senior High School Course", "60.000", Integer.toString(R.drawable.btn_code),
+                    "Jl. Tinggi Amat 3", "Kami memberikan jaminan masuk PTN, tidak masuk PTN uang kembali 99%.",
+                    "Certif", "Tersedia", "Senin - Jumat"},
+            {"Raveraldz's Scuba Dive", "150.000", Integer.toString(R.drawable.btn_code), "Jl. Selam Dalam 9",
+                    "Kami memberikan instruktur selam untuk anda, dengan fasilitas sewa perlatan menyelam" +
+                    " gratis selama kursus.", "Certif", "Tersedia", "Sabtu - Minggu, Libur Nasional"},
+            {"Raveraldz's Bonsai", "100.000", Integer.toString(R.drawable.btn_code), "Jl. Potong Rapi 7",
+                    "Kami memberikan pelatihan melakukan bonsai kepada tanaman anda. Kami memberikan" +
+                    " jaminan kenyamanan untuk Anda.", "Certif", "Tersedia", "Senin - Jumat"}
     };
 
     protected KursusData(Parcel in) {
